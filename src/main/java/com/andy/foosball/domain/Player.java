0716cf21bc0accb50c08bff1ed19d8afff9242b5
andy <202007621@post.au.dk>
2025-10-06 15:49:02 +0200

    Added static factory method in Player

--- src/main/java/com/andy/foosball/domain/Player.java
+++ src/main/java/com/andy/foosball/domain/Player.java
@@ -6,6 +6,15 @@ public class Player {
     private String initials;
     private int handicap = 10;
 
+    private Player(String name, String initials) {
+        this.name = name;
+        this.initials = initials;
+    }
+
+    public static Player of(String name, String initials) {
+        return new Player(name, initials);
+    }
+
     public void setName(String newName) {
         name = newName;
     }
