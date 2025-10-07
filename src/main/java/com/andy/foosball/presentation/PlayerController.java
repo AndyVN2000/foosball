d6c5c8908988e7d2a94c28d5373acea3a4fe2f32
andy <202007621@post.au.dk>
2025-10-07 14:40:22 +0200

    Written POST method for PlayerController. Not tested yet

--- src/main/java/com/andy/foosball/presentation/PlayerController.java
+++ src/main/java/com/andy/foosball/presentation/PlayerController.java
@@ -1,10 +1,14 @@
 package com.andy.foosball.presentation;
 
 import com.andy.foosball.application.PlayerApplicationService;
+import com.andy.foosball.domain.Player;
 import com.andy.foosball.domain.PlayerRepository;
+
+import java.util.OptionalInt;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 /**
  * https://spring.io/guides/tutorials/rest
@@ -18,6 +22,13 @@ public class PlayerController {
         this.playerApplicationService = new PlayerApplicationService(playerRepository);
     }
 
-
+    @PostMapping("/players")
+    Player newPlayer(@RequestBody Player player) {
+        playerApplicationService.createPlayer(
+            player.getName(),
+            player.getInitials(),
+            OptionalInt.of(player.getHandicap()));
+        return player;
+    }
 
 }
