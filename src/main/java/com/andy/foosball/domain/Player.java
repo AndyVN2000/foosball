5099daa0e0b1e2b68a22aee11702af99a553bc8a
andy <202007621@post.au.dk>
2025-10-07 14:16:50 +0200

    Added JPA annotations in Player.java

--- src/main/java/com/andy/foosball/domain/Player.java
+++ src/main/java/com/andy/foosball/domain/Player.java
@@ -1,10 +1,23 @@
 package com.andy.foosball.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+
+/**
+ * Der blev ikke specificeret en primary key, men jeg synes at `name` er 
+ * en passende primary key, da den bruges til at søge efter spillere.
+ * En anden overvejelse jeg har lavet er at erklære en dedikeret `id`
+ * feltvariabel af typen `UUID`. 
+ * Dog er det ikke en del af business rules, så jeg er usikker på hvorvidt
+ * det er en gyldig løsning.
+ */
+@Entity
 public class Player {
 
-    private String name;
-    private String initials;
-    private int handicap = 10;
+    private @Id String name;
+    private @Column String initials;
+    private @Column int handicap = 10;
 
     private Player(String name, String initials, int handicap) {
         this.name = name;
