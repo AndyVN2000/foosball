1de6e5d23958ba53fb22f16301059de4040bfd50
andy <202007621@post.au.dk>
2025-10-07 11:46:53 +0200

    Commented out generated test and and working on the PlayerController

--- src/main/java/com/andy/foosball/presentation/PlayerController.java
+++ src/main/java/com/andy/foosball/presentation/PlayerController.java
@@ -1,5 +1,19 @@
 package com.andy.foosball.presentation;
 
+import com.andy.foosball.application.PlayerApplicationService;
+import com.andy.foosball.domain.PlayerRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class PlayerController {
+    PlayerRepository playerRepository;
+    PlayerApplicationService playerApplicationService;
+
+    public PlayerController(PlayerRepository playerRepository) {
+        this.playerApplicationService = new PlayerApplicationService(playerRepository);
+    }
+
+
 
 }
--- src/test/java/com/foosball/foosball/FoosballApplicationTests.java
+++ src/test/java/com/foosball/foosball/FoosballApplicationTests.java
@@ -3,11 +3,11 @@ package com.foosball.foosball;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FoosballApplicationTests {
+// @SpringBootTest
+// class FoosballApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
