8190046cf9a6f35e5536e2613edaa0966402adab
andy <202007621@post.au.dk>
2025-10-19 22:53:27 +0200

    Added a LoadDatabase.java that should populate the database when starting the app. Does not work for now. Must learn on how to inject dependencies using Spring

---
+++ src/main/java/com/andy/foosball/LoadDatabase.java
@@ -0,0 +1,26 @@
+package com.andy.foosball;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.andy.foosball.domain.Player;
+import com.andy.foosball.domain.PlayerRepository;
+
+
+/**
+ * Inspired by: https://spring.io/guides/tutorials/rest
+ */
+
+@Configuration
+public class LoadDatabase {
+
+@Bean
+CommandLineRunner initDatabase(PlayerRepository repository) {
+    return args -> {
+        System.out.println("-------------- INITIALIZING DATABASE ------------------");
+        repository.save(Player.createPlayerWithDefaultHandicap("Foo Bar", "F.B"));
+    };
+}
+
+}
