f6839741924a7f6434471a541d64a4313162d01b
andy <202007621@post.au.dk>
2025-10-20 14:49:23 +0200

    Added method in TeamApplicationService for setting up teams

--- src/main/java/com/andy/foosball/application/TeamApplicationService.java
+++ src/main/java/com/andy/foosball/application/TeamApplicationService.java
@@ -1,7 +1,11 @@
 package com.andy.foosball.application;
 
+import com.andy.foosball.application.exceptions.PlayerNotFoundException;
+import com.andy.foosball.domain.Match;
+import com.andy.foosball.domain.Player;
 import com.andy.foosball.domain.MatchRepository;
 import com.andy.foosball.domain.PlayerRepository;
+import com.andy.foosball.domain.Team;
 
 /**
  * This class is responsible for the orchestration and setup of matches and its players
@@ -11,6 +15,19 @@ public class TeamApplicationService {
     private MatchRepository matchRepository;
     private PlayerRepository playerRepository;
 
-    
+    public TeamApplicationService(MatchRepository matchRepository, PlayerRepository playerRepository) {
+        this.matchRepository = matchRepository;
+        this.playerRepository = playerRepository;
+    }
+
+    public void addPlayersToTeamInMatch(String player0Name, String player1Name, Team team, int matchId) {
+        Match match = matchRepository.getReferenceById(matchId);
+        Player player0 = playerRepository.findByName(player0Name)
+            .orElseThrow(() -> new PlayerNotFoundException());
+        Player player1 = playerRepository.findByName(player1Name)
+        .orElseThrow(() -> new PlayerNotFoundException());
+
+        match.setTeam(team, player0, player1);
+    }
 
 }
